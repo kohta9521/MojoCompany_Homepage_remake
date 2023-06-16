d11a3997fe2117e51b748baf65f32a1c7775afc1
Kohta9521 <kohta0421kochi@gmail.com>
2023-06-16 12:52:42 +0900

    add header components and header features compoents

---
+++ src/layout/header/Header.jsx
@@ -0,0 +1,13 @@
+import header from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className={header.main}>
+      samplecode
+      </div>
+    </>
+  )
+}
+
+export default Header;
