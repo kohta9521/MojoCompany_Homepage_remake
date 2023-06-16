41ede67d6c1e6ce2ce0168254a09e865b97cdba9
Kohta9521 <kohta0421kochi@gmail.com>
2023-06-16 13:04:34 +0900

    add logo compoents

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,9 +1,17 @@
-import logo from "./Logo.module.css";
+import styles from "./Logo.module.css";
+
+import Link from "next/link";
+
 
 const Logo = () => {
   return (
     <>
-      logoarea
+      <div className={styles.logo}>
+        <Link href="/">
+          {/* <a className={styles.logoitem} href="">ハンバーガー</a> */}
+          <p className={styles.logoitem}>mojo</p>
+        </Link>
+      </div>
     </>
   )
 }
